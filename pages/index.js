84a867c783ffac0bc95343f8854d808847a969ba
Eduardo <lalopx555@gmail.com>
2022-03-30 12:20:55 -0600

    Implementacion de la funcion de react -getSession-

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,45 @@
-import {getSession} from 'next-auth/react';
-import {useEffect, useState} from 'react';
+import { getSession } from 'next-auth/react';
 
 
 
-function HomePage() {
 
-  const [user, setUser] = useState(null)
+  function HomePage({ session }) {
 
-  useEffect(() => {
-    (async() => {
-      const session = await getSession()
-      console.log(session);
-      setUser(session.user)
-    })();
-  }, [])
+    const {user} = session;
+
+      return (
+      <div>
+        <h1>{user.name}</h1>
+        <p>{user.email}</p>
+        <img src={user.image} alt=""></img>
+      </div>
+    );
+  }
+  
+
+  export const getServerSideProps = async (context) => {
+
+    const session = await getSession(context);
+
+    return {
+      props: {
+        session: session,
+      },
+    };
+
+  };
+
+  export default HomePage;
 
 
-  return (
-    <div>
+  
+    //////////Manera de hacerlo con el uso de useEffect////
 
-      {JSON.stringify(user)}
-
-      <h1>{user.name}</h1>
-      <p>{user.email}</p>
-      <img src={user.image} alt=""></img>
-
-    </div>
-  );
-}
-
-export default HomePage;
+    //const [user, setUser] = useState(null)
+  // useEffect(() => {
+    // (async() => {
+      // const session = await getSession()
+        //console.log(session);
+        //setUser(session.user)
+      //})();
+    //}, [])
