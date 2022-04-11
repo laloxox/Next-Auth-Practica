83564fc8835d5b5bea0ef4f81a18140938811114
Eduardo <lalopx555@gmail.com>
2022-04-11 14:03:07 -0500

    Se agregaron las variables de entorno NEXTATH_SECRET y NEXTAUTH_URL

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
-import { useSession } from 'next-auth/react';
-import {useRouter} from 'next/router'
+import { getSession } from 'next-auth/react'
 
-  function HomePage() {
 
-    const {data: session, status} = useSession()
-    const router = useRouter()
-      
 
-      if (status === 'loading') {
-        return <p>Loading...</p>
-      }
-
-      if (status === 'unauthenticated') {
-        router.push('/login')
-      }
+  function HomePage({session}) {
 
       return (
       <div>
@@ -38,10 +27,42 @@ import {useRouter} from 'next/router'
     );
   }
 
+  export const getServerSideProps = async (context) => {
+
+    const session = await getSession(context)
+
+    if (!session) return { 
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+
+    return {
+      props: {
+        session
+      }
+    }
+  }
+
   export default HomePage;
 
 
 
+
+
+
+   // const {data: session, status} = useSession()
+    // const router = useRouter()
+      
+
+    //   if (status === 'loading') {
+    //     return <p>Loading...</p>
+    //   }
+
+    //   if (status === 'unauthenticated') {
+    //     router.push('/login')
+    //   }
 
 
 
